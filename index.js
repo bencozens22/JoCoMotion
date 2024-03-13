2c51e56f32757e1820a56df1c2083e1ab9541e25
unknown <bencozens22@gmail.com>
2024-03-13 22:51:24 +0000

    Adding javascript so the get in touch button scrolls down to the contact form

---
+++ index.js
@@ -0,0 +1,7 @@
+function scrollToContactForm() {
+    // Get the contact form section element
+    var contactFormSection = document.getElementById("contact-form-section");
+
+    // Scroll to the contact form section using smooth behavior
+    contactFormSection.scrollIntoView({ behavior: 'smooth' });
+}
